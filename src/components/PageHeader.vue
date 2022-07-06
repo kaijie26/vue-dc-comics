eb06f83fda9f2c79d59be789235a4e81ab2ffd80
kaijie26 <chenkaijie26@gmail.com>
2022-07-06 22:50:36 +0200

    create basic files

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,23 @@
+<!-- HTML -->
+<template>
+    <header>
+        
+        
+    </header>
+
+</template>
+
+
+<!-- JAVASCRIPT -->
+<script>
+export default {
+    name: "PageHeader",
+    
+}
+</script>
+
+
+<!-- CSS -->
+<style lang="scss" scoped>
+
+</style>
