d1660aea210b6d66beccc5cc993c93f8414b9f80
kaijie26 <chenkaijie26@gmail.com>
2022-07-08 00:09:58 +0200

    dinamic card

---
+++ src/components/SeriesCard.vue
@@ -0,0 +1,40 @@
+<!-- HTML -->
+<template>
+    <div class="card">
+        <img :src="infoCard.thumb" :alt="infoCard.type">
+
+        <h3>{{ infoCard.series}}</h3>
+
+    </div>
+        
+        
+    
+</template>
+
+<!-- JAVASCRIPT -->
+<script>
+export default {
+    name: 'SeriesCard',
+    props: {
+        infoCard: Object
+    }
+    
+}
+</script>
+
+<!-- CSS -->
+<style lang="scss" scoped>
+.card{
+    width: calc((100% / 6) - 20px);
+    margin: 10px;
+
+    img{
+        width: 100%;
+        aspect-ratio: 1/1;
+        object-fit: cover;
+        object-position: top;
+    }
+
+}
+
+</style>
